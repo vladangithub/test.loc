c84e23def4db84e40a63376185a42a71c6bebcd8
azesm <azesm@yahoo.com>
2023-03-27 13:54:53 +0300

    15while

--- 13IfElse/index.php
+++ 13IfElse/index.php
@@ -1,5 +1,5 @@
 <?php error_reporting(-1);
-<<<<<<< HEAD
+
 // https://www.php.net/manual/ru/language.control-structures.php
 
 // echo 'Test 1', 'Test 2', 'Test 3';
@@ -41,20 +41,6 @@ if ($light == "red"){
     echo 'Stop';
 }*/
 
-if ($light == "green") {
-    echo 'You can go now';
-} elseif ($light == "yellow") {
-    echo 'Get ready';
-}  elseif ($light == "red") {
-    echo 'Stop';
-    if (2 > 3) {
-        echo '2 > 3';
-    } else {
-        echo '2 < 3';
-    }
-} else {
-    echo 'Run';
-}
 
 
 
@@ -87,7 +73,3 @@ if ($light == "green") {
 
 
 
-
-
-=======
->>>>>>> verTwo
--- 14ternaryOperator/index.php
+++ 14ternaryOperator/index.php
@@ -1,1 +1,35 @@
 <?php error_reporting(-1);
+
+
+/*var_dump($price);*/
+
+$light = 'green';
+
+/*if ($light == 'green') {
+    echo 'you can go now';
+} else {
+    echo 'stop';
+}*/
+
+
+// echo ($light == 'green') ? 'you can go now' : 'stop';
+
+$price = 100;
+
+// echo $price ? $price : 'unknow price';
+// echo $price ?: 'unknown price';
+
+$test = $price >= 10 ?: 'unknown price';
+var_dump($test);
+
+
+
+
+
+
+
+
+
+
+
+
